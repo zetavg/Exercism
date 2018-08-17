f44b5ac099be6850a6d0d01da58af85193f20312
zetavg <mail@zeta.vg>
2018-08-17 16:52:42 +0800

    haskell: hamming: improve code style

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,8 +1,13 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance [] [] = Just 0
-distance (x:xs) [] = Nothing
-distance [] (y:ys) = Nothing
-distance (x:xs) (y:ys) = case distance xs ys of Just d -> Just (if x == y then d else 1 + d)
-                                                Nothing -> Nothing
+distance []     []     = Just 0
+distance (x:xs) []     = Nothing
+distance []     (y:ys) = Nothing
+distance (x:xs) (y:ys) =
+  case distance xs ys of
+    Just d  -> Just (d + distanceOfHead)
+    Nothing -> Nothing
+  where
+    distanceOfHead | x == y    = 0
+                   | otherwise = 1
