5ea13124355e54a2395a7143dda52a4a8416bdca
zetavg <mail@zeta.vg>
2018-08-17 16:44:11 +0800

    haskell: solve hamming

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,8 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance [] [] = Just 0
+distance (x:xs) [] = Nothing
+distance [] (y:ys) = Nothing
+distance (x:xs) (y:ys) = case distance xs ys of Just d -> Just (if x == y then d else 1 + d)
+                                                Nothing -> Nothing
