7ad3ca4b2fd13503ace2c8ee2cef0ed51632295e
zetavg <mail@zeta.vg>
2018-08-18 14:57:35 +0800

    haskell: solve perfect-numbers

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -3,4 +3,14 @@ module PerfectNumbers (classify, Classification(..)) where
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify x
+  | x <= 0 = Nothing
+  | aliquotSum x == x = Just Perfect
+  | aliquotSum x > x = Just Abundant
+  | aliquotSum x < x = Just Deficient
+
+aliquotSum :: Int -> Int
+aliquotSum = sum.init.factors
+
+factors :: Int -> [Int]
+factors x = [y | y <- [1..x], x `mod` y == 0]
