322c8e348adaee5fb1bf2f58df432ab248eb5d7c
zetavg <mail@zeta.vg>
2018-08-19 23:37:49 +0800

    haskell: leap: improve code style

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,9 +2,9 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | year `divisibleBy` 400 = True
-  | year `divisibleBy` 100 = False
-  | year `divisibleBy` 4   = True
-  | otherwise              = False
+  | yearDivisibleBy 400 = True
+  | yearDivisibleBy 100 = False
+  | yearDivisibleBy 4   = True
+  | otherwise           = False
   where
-    x `divisibleBy` y = x `mod` y == 0
+    yearDivisibleBy x = year `mod` x == 0
