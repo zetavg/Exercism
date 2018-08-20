8b7e230e628991d0f9beb2989bdbb1ffaf753654
zetavg <mail@zeta.vg>
2018-08-20 19:47:20 +0800

    haskell: solve allergies

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -8,10 +8,34 @@ data Allergen = Eggs
               | Chocolate
               | Pollen
               | Cats
-              deriving (Eq)
+              deriving (Eq, Show)
+
+listOfAllAllergies =
+  [ Eggs
+  , Peanuts
+  , Shellfish
+  , Strawberries
+  , Tomatoes
+  , Chocolate
+  , Pollen
+  , Cats ]
+
+bits :: Int -> [Bool]
+bits number
+  | number <= 0 = []
+  | mod == 1 = True : bits div
+  | mod == 0 = False : bits div
+  where (div, mod) = number `divMod` 2
+
+allergiesOfBits :: [Bool] -> [Allergen]
+allergiesOfBits =
+  map (\(Just a) -> a)
+    . filter (/= Nothing)
+    . map (\(a, b) -> if b then Just a else Nothing)
+    . zip listOfAllAllergies
 
 allergies :: Int -> [Allergen]
-allergies score = error "You need to implement this function."
+allergies = allergiesOfBits . bits
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = error "You need to implement this function."
+isAllergicTo allergen score = allergen `elem` allergies score
