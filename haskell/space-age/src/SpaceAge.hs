9bd1db2f1990108c26e92b776dac07cfb94f810b
zetavg <mail@zeta.vg>
2018-09-27 16:30:56 +0800

    haskell: submit space-age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,14 +10,17 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn Mercury seconds = yearsOfSecondsOnEarth seconds / 0.2408467
-ageOn Venus   seconds = yearsOfSecondsOnEarth seconds / 0.61519726
-ageOn Earth   seconds = yearsOfSecondsOnEarth seconds
-ageOn Mars    seconds = yearsOfSecondsOnEarth seconds / 1.8808158
-ageOn Jupiter seconds = yearsOfSecondsOnEarth seconds / 11.862615
-ageOn Saturn  seconds = yearsOfSecondsOnEarth seconds / 29.447498
-ageOn Uranus  seconds = yearsOfSecondsOnEarth seconds / 84.016846
-ageOn Neptune seconds = yearsOfSecondsOnEarth seconds / 164.79132
+ageOn planet seconds = yearsOfSecondsOnEarth seconds / orbitalPeriodOfEarthYears planet
 
 yearsOfSecondsOnEarth :: Float -> Float
 yearsOfSecondsOnEarth seconds = seconds / 31557600.0
+
+orbitalPeriodOfEarthYears :: Planet -> Float
+orbitalPeriodOfEarthYears Earth   = 1
+orbitalPeriodOfEarthYears Mercury = 0.2408467
+orbitalPeriodOfEarthYears Venus   = 0.61519726
+orbitalPeriodOfEarthYears Mars    = 1.8808158
+orbitalPeriodOfEarthYears Jupiter = 11.862615
+orbitalPeriodOfEarthYears Saturn  = 29.447498
+orbitalPeriodOfEarthYears Uranus  = 84.016846
+orbitalPeriodOfEarthYears Neptune = 164.79132
